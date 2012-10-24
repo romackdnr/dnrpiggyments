0b91259135b01626501edea1a2e267f16b199650
Romack Natividad <romack@dogandrooster.com>
2012-10-24 08:31:03 +0800

    refactored codes

--- dev/index.php
+++ dev/index.php
@@ -1,84 +1,79 @@
-<?php include 'manager/_pi/base.php'; ?>
-<?
-	$page1 = Pages::findPages(1);
+<?php 
+include 'manager/_pi/base.php';
+$page1 = Pages::findPages(1);
 ?>
-	<? startblock('section') ?>
-  <aside>
-   		<?php include 'manager/includes/side_panel.php'; ?>
-  </aside>
-  <article>
-  	<div id=hp_hdr>
+
+<? startblock('section') ?>
+<aside>
+	<?php include 'manager/includes/side_panel.php'; ?>
+</aside>
+<article>
+  	<div id="hp_hdr">
     	<img src="assets/images/ajax_header.jpg">      
     </div>
     
-    <div id=featured_products>
-    	<hgroup>
-      	<h2>Featured Products</h2>
-        <hr />
-      </hgroup>
-      <?
-	  		$product = Products::displayAllFeaturedProducts();
-			foreach($product as $products) { 
-				$nctr=$nctr+1;
-				if($nctr==2) {
-					$pClass = "class='last'";
-				} else {
-					$pClass = "";
-				}
-	  ?>
-      	<? if($nctr==1) { ?><ul><? } ?>
-      	<li <?=$pClass?>><dl>
-        	<dt><img src="<?=$ROOT_URL?>products_image/<?=$products->fldProductsImage?>" alt="<?=$products->fldProductsName?>" width="88"></dt>
-          <dd>
-          	<big>$<?=number_format($products->fldProductsPrice,2)?></big>
-            <a href="products-details-<?=$products->fldProductsId?>.html" class=details>View Details</a> &nbsp; 
-            <? $optionExists = TempCart::checkProductOption($products->fldProductsId);?>
-            <? if ($optionExists==0): ?>
-                <a href="shopping-cart-product-<?=$products->fldProductsId?>.html" class=cart>Add to Cart</a>
-            <? endif; ?>
-            <h3><?=$products->fldProductsName?></h3>
-            <?=substr($products->fldProductsOverview,0,75)?> [...]
-          </dd>
-        </dl></li>
-      <? if($nctr==2) {$nctr=0; ?></ul><? } ?>  
-     <? } ?>   
+    <div id="featured_products">
+        <hgroup>
+            <h2>Featured Products</h2>
+            <hr />
+        </hgroup>
+        <? $product = Products::displayAllFeaturedProducts(); ?>
+        <? foreach($product as $products):
+            $nctr=$nctr+1;
+            $pClass = ($nctr==2) ? "class='last'" : "";?>
+
+            <? if($nctr==1) { ?><ul><? } ?>
+            <li <?=$pClass?>><dl>
+                <dt>
+                    <img src="<?=$ROOT_URL?>products_image/<?=$products->fldProductsImage?>" alt="<?=$products->fldProductsName?>" width="88">
+                </dt>
+                <dd>
+                    <big>$<?=number_format($products->fldProductsPrice,2)?></big>
+                    <a href="products-details-<?=$products->fldProductsId?>.html" class=details>View Details</a> &nbsp; 
+                    <? $optionExists = TempCart::checkProductOption($products->fldProductsId);?>
+                    <? if ($optionExists==0): ?>
+                    <a href="shopping-cart-product-<?=$products->fldProductsId?>.html" class=cart>Add to Cart</a>
+                    <? endif; ?>
+                    <h3><?=$products->fldProductsName?></h3>
+                    <?=substr($products->fldProductsOverview,0,75)?> [...]
+                </dd>
+                </dl>
+            </li>
+            <? if($nctr==2) {$nctr=0; ?></ul><? } ?>  
+        <? endforeach ?> 
            
-      <div class='clear right'>
-      	<a href="<?=$root?>products-all.html">View all products &raquo;</a>
-      </div>      
+        <div class='clear right'>
+            <a href="<?=$root?>products-all.html">View all products &raquo;</a>
+        </div>      
     </div>
     
     <hr />
     
-    <ul id=home_info_panel>
-    	<li>
-      	<hgroup>
-        	<h2>About Us</h2>
-          <hr />
-        </hgroup>
-        <blockquote>
-        	<?=substr($page1->fldPagesDescription,0,200)?>... <a href="<?=$root?>about-us.html">Read more</a>
-        </blockquote>
-      </li>
-      <li class=last>
-      	<hgroup>
-        	<h2>Customer Testimonials</h2>
-          <hr />
-        </hgroup>
-        <? $testi = Testimonials::findTestimonialsHome();?>
-        <blockquote>
-        	<?=$testi->fldTestimonialsDescription?>
-          <!-- This section is for Testimonial Submitter. Do not move or re-place tag. -->
-          <div class=testi_user> <strong><?=$testi->fldTestimonialsName?></strong>, <?=$testi->fldTestimonialsWebsite?></div>
-        </blockquote>
-      </li>    
+    <ul id="home_info_panel">
+        <li>
+            <hgroup>
+                <h2>About Us</h2>
+                <hr />
+            </hgroup>
+            <blockquote>
+                <?=substr($page1->fldPagesDescription,0,200)?>... <a href="<?=$root?>about-us.html">Read more</a>
+            </blockquote>
+        </li>
+        <li class="last">
+            <hgroup>
+            <h2>Customer Testimonials</h2>
+            <hr />
+            </hgroup>
+            <? $testi = Testimonials::findTestimonialsHome();?>
+            <blockquote>
+                <?=$testi->fldTestimonialsDescription?>
+                <!-- This section is for Testimonial Submitter. Do not move or re-place tag. -->
+                <div class="testi_user"> <strong><?=$testi->fldTestimonialsName?></strong>, <?=$testi->fldTestimonialsWebsite?></div>
+            </blockquote>
+        </li>    
     </ul>    
-  </article>
-	<? endblock() //end section ?>
-
-
-
-
+</article>
+<? endblock() //end section ?>
 
 <? startblock('headercodes') ?>
 <? endblock() //end headercodes ?>
--- dev/manager/includes/account_panel.php
+++ dev/manager/includes/account_panel.php
@@ -1,14 +1,14 @@
  <h2>My Account</h2>
-    <menu>
-       
-	      		<li><a href="account-billing-information.html">Billing Information</a></li>
-                <li><a href="account-shipping-information.html">Shipping Information</a></li>
-                <li><a href="account-orders-information.html">My Order</a></li>
-                
-    </menu>
-
-    <h2>Follow Us</h2>
-    <ul class=socialnetwork>
-    	<li><a href="#"><img src="assets/images/social-facebook.png" width="24" height="24" alt="Facebook"> Become a Fan</a></li>
-      <li><a href="#"><img src="assets/images/social-twitter.png" width="24" height="24" align="Twitter"> Follow us on Twitter</a></li>
-    </ul>
+<menu>
+    <li><a href="account-billing-information.html">Billing Information</a></li>
+    <li><a href="account-shipping-information.html">Shipping Information</a></li>
+    <li><a href="account-orders-information.html">My Order</a></li>
+</menu>
+<h2>Follow Us</h2>
+<ul class="socialnetwork">
+    <li><a href="#"><img src="assets/images/social-facebook.png" width="24" height="24" alt="Facebook"> Become a Fan</a></li>
+    <li><a href="#"><img src="assets/images/social-twitter.png" width="24" height="24" align="Twitter"> Follow us on Twitter</a></li>
+</ul>
+<pre>
+    <? print_r($_SESSION); ?>
+</pre>
